36aa39d77cc959022b839fb0da6042350cea0a24
paalso <40a.pvl@gmail.com>
2020-12-19 22:00:35 +0200

    8:9

--- 8/8-9.py
+++ 8/8-9.py
@@ -7,9 +7,19 @@
 
 from itertools import permutations
 
-##print(*permutations([1,2,3]))
-##print('\n'.join(list(permutations([1,2,3]))))
-
-l = range(1, int(input()) + 1)
-x = list(permutations(l))
-print(x)
+print(
+    "\n".join(
+        map(
+            lambda e: "".join(e),
+            permutations(
+                map(
+                    str,
+                    range(
+                        1,
+                        int(input()) + 1
+                    )
+                )
+            )
+        )
+    )
+)
